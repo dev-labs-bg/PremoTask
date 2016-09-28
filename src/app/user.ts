002bfba614091089cde4af03b1a551d5a7e9eb72
superKalo <superkalo@devlabs.bg>
2016-09-29 00:44:23 +0300

    Added: users data class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,7 +1,8 @@
 export class User {
     constructor(
         public id:number,
-        public name:string,
+        public first_name:string,
+        public last_name:string,
         public country_id:number,
         public email:string
     ) {}
