97225ced4fce268c876d310788915cf977213d30
superKalo <superkalo@devlabs.bg>
2016-09-28 13:13:59 +0300

    Added: dashboard component structure

--- src/app/module.ts
+++ src/app/module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { App } from './app';
 import { HeaderComponent } from './header.component';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
     imports: [
@@ -9,7 +10,8 @@ import { HeaderComponent } from './header.component';
     ],
     declarations: [
         App,
-        HeaderComponent
+        HeaderComponent,
+        DashboardComponent
     ],
     bootstrap: [ App ]
 })
