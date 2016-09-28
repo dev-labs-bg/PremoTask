13ec113000009d038307d8e0cb583a5e75275ceb
superKalo <superkalo@devlabs.bg>
2016-09-28 21:37:53 +0300

    Refactored: users service

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -17,15 +17,33 @@ export class UsersService {
         private httpService: HttpService
     ) { }
 
-    private getAllUsers() {
+    private fetchAllUsers() {
         this.httpService.getData('get-all-users').subscribe(
             users => {
                 this.users = users;
-                this.usersReceived.emit(users)
+
+                this.usersReceived.emit();
             }
         );
     }
 
+    /**
+     * Trigger all users fetch,
+     * only if the current users array is empty.
+     */
+    private getAllUsers(){
+        if (! this.users.length) {
+            this.fetchAllUsers();
+        } else {
+            this.usersReceived.emit();
+        }
+    }
+
+    /**
+     * Get an array with users, filtered by country.
+     *
+     * @param {number} country - country id
+     */
     private getAllUsersByCountry(country:number){
         if (country === ALL_COUNTRIES) {
             return this.users;
@@ -34,12 +52,23 @@ export class UsersService {
         return _.filter(this.users, { country_id: country });
     }
 
+    /**
+     * Get random users (winners), using lodash's sampleSize method,
+     * https://lodash.com/docs/4.16.2#sampleSize
+     *
+     * @param {any}    users - filtered users // TODO: type!
+     * @param {number} count - the size of the random collection
+     */
+    private getRandomWinners(users:any, count:number) {
+        return _.sampleSize(users, count);
+    }
+
     drawWinners(count:number, country:number){
         this.getAllUsers();
 
         this.usersReceived.subscribe(
             () => {
-                this.winners = _.sampleSize(
+                this.winners = this.getRandomWinners(
                     this.getAllUsersByCountry(country), count
                 );
 
