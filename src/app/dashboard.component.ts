fedd27aac4b338af71a155cb57c6c47b172cffef
superKalo <superkalo@devlabs.bg>
2016-09-28 14:23:45 +0300

    Added: pull and display countries in the drop-down list

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,7 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HttpService } from "./http.service";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'dashboard.component.html'
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+    countries: { id: number, name: string }[] = [];
+
+    constructor(private httpService: HttpService) {}
+
+    ngOnInit() {
+        this.httpService.getData('get-all-countries').subscribe(
+            (data:any) => {
+                this.countries = data;
+            }
+        )
+    }
+
+    onCountryChange(newCountry:string){
+        console.log(newCountry);
+    }
+}
