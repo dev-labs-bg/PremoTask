caa8a4cec83487b56f36d506792488ae20bc558a
superKalo <superkalo@devlabs.bg>
2016-09-29 11:20:48 +0300

    Fixed: webpack production configuration

--- config-js/webpack.dev.js
+++ config-js/webpack.dev.js
@@ -1,7 +1,10 @@
+var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
+
+process.env.ENV = 'development';
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'inline-source-map',
@@ -14,6 +17,11 @@ module.exports = webpackMerge(commonConfig, {
   },
 
   plugins: [
-    new ExtractTextPlugin('[name].css')
+    new ExtractTextPlugin('[name].css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+          'ENV': JSON.stringify(process.env.ENV)
+      }
+    })
   ],
 });
